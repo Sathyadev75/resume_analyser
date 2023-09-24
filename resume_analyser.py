bedb25d5a46917c19cf0489878eecc42e0cc94bc
Sathyadev75 <119954025+Sathyadev75@users.noreply.github.com>
2023-09-24 19:03:09 +0530

    Update resume_analyser.py

--- resume_analyser.py
+++ resume_analyser.py
@@ -6,6 +6,11 @@ st.title("PDF Uploader and Saver")
 # Upload a PDF file
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
+st.title("Smart Resume Analyser")
+st.sidebar.markdown("# Choose User")
+activities = ["Normal User", "Admin"]
+choice = st.sidebar.selectbox("Choose among the given options:", activities)
+
 if uploaded_file is not None:
     # Display the uploaded file
     st.write("Uploaded PDF file:", uploaded_file.name)
@@ -16,7 +21,3 @@ if uploaded_file is not None:
     
     st.success(f"File '{uploaded_file.name}' saved successfully by prakash!")
     
-st.title("Smart Resume Analyser")
-st.sidebar.markdown("# Choose User")
-activities = ["Normal User", "Admin"]
-choice = st.sidebar.selectbox("Choose among the given options:", activities)
