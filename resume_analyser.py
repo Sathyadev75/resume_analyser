e97602fdcd1317280bdff2c0ff880eb787c134a2
Sathyadev75 <119954025+Sathyadev75@users.noreply.github.com>
2023-09-24 19:05:34 +0530

    Update resume_analyser.py

--- resume_analyser.py
+++ resume_analyser.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
+
+st.title("Smart Resume Analyser")
+st.sidebar.markdown("# Choose User")
+activities = ["Normal User", "Admin"]
+choice = st.sidebar.selectbox("Choose among the given options:", activities)
+
 # Create a Streamlit app
 st.title("PDF Uploader and Saver")
 
 # Upload a PDF file
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
-st.title("Smart Resume Analyser")
-st.sidebar.markdown("# Choose User")
-activities = ["Normal User", "Admin"]
-choice = st.sidebar.selectbox("Choose among the given options:", activities)
+
 
 if uploaded_file is not None:
     # Display the uploaded file
