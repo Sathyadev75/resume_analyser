445694d545b7656501c1231b238edf96d7a45137
Sathyadev75 <119954025+Sathyadev75@users.noreply.github.com>
2023-09-25 12:52:11 +0530

    Update resume_analyser.py

--- resume_analyser.py
+++ resume_analyser.py
@@ -1,26 +1,34 @@
 import streamlit as st
 
+import fitz  # PyMuPDF
 
 st.title("Smart Resume Analyser")
 st.sidebar.markdown("# Choose User")
 activities = ["Normal User", "Admin"]
 choice = st.sidebar.selectbox("Choose among the given options:", activities)
 
-# Create a Streamlit app
-st.title("PDF Uploader and Saver")
-
 # Upload a PDF file
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
-
-
 if uploaded_file is not None:
-    # Display the uploaded file
+    # Display the uploaded PDF file name
     st.write("Uploaded PDF file:", uploaded_file.name)
 
     # Save the uploaded file to your PC
-    with open(uploaded_file.name, "wb") as f:
+    file_name = f"uploaded_resume.pdf"  # Choose a suitable name
+    with open(file_name, "wb") as f:
         f.write(uploaded_file.read())
-    
-    st.success(f"File '{uploaded_file.name}' saved successfully by prakash!")
-    
+
+    st.success(f"File '{file_name}' saved successfully!")
+
+    # Use PyMuPDF to convert PDF pages to images
+    pdf_document = fitz.open(file_name)
+    pdf_images = []
+    for page_number in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_number)
+        pdf_image = page.get_pixmap()
+        pdf_images.append(pdf_image)
+
+    # Display PDF pages as images
+    for page_number, pdf_image in enumerate(pdf_images):
+        st.image(pdf_image, caption=f"Page {page_number + 1}", use_column_width=True)
